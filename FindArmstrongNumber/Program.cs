c3f890497237ffbfb42140bf4e20bb1602624e38
nidakul <nidakul@gmail.com>
2023-10-28 00:41:00 +0300

    Initial commit

---
+++ FindArmstrongNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+List<int> numbers = new List<int>();
+int pow;
+int sum = 0;
+
+int sayı;
+
+for (int i = 10; i <= number * 10; i *= 10)
+{
+    numbers.Add(number % i);
+}
+FindArmstrongNumbers();
+
+void FindArmstrongNumbers()
+{
+    int multiple = 1;
+    for (int j = 0; j < numbers.Count; j++)
+    {
+        sayı = numbers[j] / multiple;
+        multiple *= 10;
+        pow = Convert.ToInt32(Math.Pow(sayı, numbers.Count));
+        sum += pow;
+    }
+} 
+
+if(sum == number)
+{
+    Console.WriteLine("The number is armstrong number");
+}
+else
+{
+    Console.WriteLine("The number is not armstrong number");
+
+}
+
+
