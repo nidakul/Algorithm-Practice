bcc37ed5bf3df444285c46f7c08a1e1f47989941
nidakul <nidakul@hotmail.com>
2023-10-29 21:56:27 +0300

    adding findFibonacciSeries

---
+++ FindFibonacciSeries/Program.cs
@@ -0,0 +1,14 @@
+﻿//fibonacci serisini yaz. (ilk 100 sayı)
+int temp;
+int initial = 1;
+int initialValue = 0;
+Console.WriteLine(initial);
+
+for (int i = 1; i < 100; i++)
+{
+    temp = initialValue;
+    initialValue = initial;
+    initial = temp + initial;
+
+    Console.WriteLine(initial);
+}
